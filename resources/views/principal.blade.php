bd331e07e0c2318b03632ea1c31c4b2d640c8a77
L0mbu1 <lomelibuitron2a@gmail.com>
2024-02-06 18:08:38 -0700

    'Hola'

--- resources/views/principal.blade.php
+++ resources/views/principal.blade.php
@@ -21,6 +21,7 @@
         Categorias mas buscadas
     </section>
     <section id="anuncios">
+        <p>Hola soy Berenice  c:</p>
         Anuncios
     </section>
     <section id="superOfertas">
