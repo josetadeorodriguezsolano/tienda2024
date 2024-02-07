642cf5b1f1c5538e862fb48b57576c2990bf313e
tadeo.rodriguez <tadeo.rs@lapaz.tecnm.mx>
2024-02-06 17:22:20 -0700

    Este es el primer commit

---
+++ resources/views/principal.blade.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=<, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+</head>
+<body>
+    <header id="encabezadoPrincipal">
+        <img src="img/logo.jpg">
+        Super Tienda en Linea
+        <section id="carrito">
+            1 productos - $1857.00
+            <img src="img/carrito.webp">
+        </section>
+        <section id="cuenta">
+            <button>Registrate</button>
+            <button>Mi cuenta</button>
+        </section>
+        <section id="busqueda">
+            <input type="text" id="txtBusqueda">
+        </section>
+        <section id="saludo">
+            Hola Jose
+        </section>
+    </header>
+    <nav>Barra de navegacion</nav>
+    <section id="masBuscado">
+        Categorias mas buscadas
+    </section>
+    <section id="anuncios">
+        Anuncios
+    </section>
+    <section id="superOfertas">
+        Super Ofertas
+    </section>
+    <section id="masVendido">
+        Mas vendido
+    </section>
+    <section id="nuevo">
+        Lo Nuevo
+    </section>
+    <section id="gamers">
+        Gamers
+    </section>
+    <section id="comentarios">
+        Comentarios
+    </section>
+    <section id="recientes">
+        Recientes
+    </section>
+    <section id="suscribirse">
+        Suscribirse
+    </section>
+    <footer>
+        Pie de pagina
+    </footer>
+</body>
+</html>
