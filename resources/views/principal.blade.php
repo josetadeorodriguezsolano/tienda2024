d82ec82835bd238df8fe8231411680d5d8cadb54
JesusAntonioRodarte <tony.rodartecastro@outlook.com>
2024-02-06 18:11:27 -0700

    'hola'

--- resources/views/principal.blade.php
+++ resources/views/principal.blade.php
@@ -31,6 +31,7 @@
         Categorias mas buscadas
     </section>
     <section id="anuncios">
+        <p>Hola soy Jesus y un niño de primaria sabe mas que yo</p>
         Anuncios
     </section>
     <section id="superOfertas">
