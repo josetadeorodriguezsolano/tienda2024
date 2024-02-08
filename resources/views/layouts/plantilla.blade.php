e918f90b22b682d8e475d8c4a2f9809322e81ced
Elian <ElianOsirisLunaGaxiola@gmail.com>
2024-02-07 18:12:18 -0700

    Hojas de estilos

---
+++ resources/views/layouts/plantilla.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    @vite(['resources/sass/app.scss',''])
+</head>
+<body>
+    @section("header")
+        @show()
+    @section("nav")
+    @section("main")
+    @section("aside")
+    @section("footer")
+</body>
+</html>
