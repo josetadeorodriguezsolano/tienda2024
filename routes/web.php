b152dec259ee3e551fec2b60555315716cc35e48
L0mbu1 <lomelibuitron2a@gmail.com>
2024-02-14 17:16:08 -0700

    'Cambios_footer'

--- routes/web.php
+++ routes/web.php
@@ -8,15 +8,6 @@ Route::get('/',[PrincipalController::class, 'paginaPrincipal']);
 Route::get('/estilos/banners',function () {
     return view('estilos.banners');
 });
-
-Route::get('/estilos/loading',function () {
-    return view('estilos.loading');
+Route::get('/footer',function () {
+    return view('layouts.componentes.footer');
 });
-// Inputs Alan
-Route::get('/estilos/inputs',function () {
-    return view('estilos.inputs');
-});
-Route::get('/tarjetas', function () {
-    return view('layouts.componentes.tarjetas');
-});
-
