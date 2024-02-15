87dd31a2973f7488c1e6cdc76329a31ebcd8f79c
JOSUEDZZ <josue.diaz324@yahoo.com>
2024-02-14 17:12:54 -0700

    'jos'

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,14 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('tienda.test');
+    return view('principal');
 });
 
 Route::get('/estilos/banners',function () {
     return view('estilos.banners');
+});
+
+
+Route::get('/estilos/loading',function () {
+    return view('estilos.loading');
 });
