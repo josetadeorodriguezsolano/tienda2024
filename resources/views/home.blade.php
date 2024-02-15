3d70c479c95d792488d83f6f869e1090eeee90e5
raulboston <raulboston10@gmail.com>
2024-02-14 17:13:20 -0700

    'pagina_principal'

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,48 +3,88 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Example Blade Template</title>
     <link rel="stylesheet" href="{{ asset('sass/home.scss') }}">
 </head>
 <body>
-    <div class="container">
-        <div class="categories">Categories</div>
-        <div class="the-collections">THE COLEST STYLES ARE HERE</div>
-        <div class="hot-deals">80% OFF <span>HOT DEALS</span></div>
-        <div class="under-99-store">EXPLORE <span>UNDER $99 STORE</span></div>
-        <div class="new-products">NEW PRODUCTS</div>
-
-        <div class="product">
-            <div class="name">Highlander M</div>
-            <div class="price">$14.00</div>
-            <div class="sale-price">$0.00</div>
+    <div id="navbar">
+    </div>
+    <div id="destacado">
+        <img src="https://www.vhv.rs/dpng/f/233-2337534_cute-ulzzang-korean-girl-model-ulzzang-korean-girl.png">
+        <h2>Winter Collection <b>2024</b></h2>
+        <h1>Conoce nuestras mejores ofertas de <b>hasta un 30%</b></h1>
+        <button id="comprar">Comprar ahora</button>
+    </div>
+    <section id="articulos">
+        <div class="articulo">
+            <div class="imagen">
+                <img src="https://img.freepik.com/foto-gratis/retrato-hermosa-modelo-mujer-morena-caucasica-sonriente-ropa-elegante-verano-rosa-azul-brillante-aislado-sobre-fondo-blanco_158538-2940.jpg"
+                    alt="">
+            </div>
+            <div class="contenido">
+                <h3>Mujer</h3>
+            </div>
         </div>
-
-        <div class="hot-deal">
-            <div class="title">ASOW</div>
-            <div class="extra-off">EXTRA $55 OFF <span>ORDER NOW</span></div>
-            <div class="products">
-                <div class="product">
-                    <div class="name">Clothing</div>
-                    <div class="price">$100.00</div>
-                    <div class="sale-price">$20.00</div>
+        <div class="articulo">
+            <div class="imagen">
+                <img src="https://i.etsystatic.com/22551544/r/il/e6037e/4120971500/il_570xN.4120971500_83yb.jpg" alt="">
+            </div>
+            <div class="contenido">
+                <h3>Accessorios</h3>
+                <i>
+                </i>
+            </div>
+        </div>
+        <div class="articulo">
+            <div class="imagen">
+                <img src="https://www.looklet.com/hubfs/NEW-HenryTile2.jpg" alt="">
+            </div>
+            <div class="contenido">
+                <h3>Hombre</h3>
+                <i>
+                </i>
+            </div>
+        </div>
+    </section>
+    <section id="tendencia">
+        <h1>Lo más top</h1>
+        <div class="tendencia-container">
+            <div class="tendencia">
+                <img src="https://zamamx.com/wp-content/uploads/2022/03/Captura-de-pantalla-2022-05-11-a-las-18.03.15.png">
+                <div class="info">
+                    <div class="nombre">
+                        <h3>Sudadera Rosa</h3>
+                    </div>
+                    <p> $5000</p>
                 </div>
-                <div class="product">
-                    <div class="name">Electronics</div>
-                    <div class="price">$100.00</div>
-                    <div class="sale-price">$20.00</div>
+            </div>
+            <div class="tendencia">
+                <img src="https://ficcus.vtexassets.com/arquivos/ids/280002/23105495901_1.jpg?v=637964388291830000">
+                <div class="info">
+                    <div class="nombre">
+                        <h3>Sudadera Rosa</h3>
+                    </div>
+                    <p> $5000</p>
                 </div>
-                <div class="product">
-                    <div class="name">Health &amp; Beauty</div>
-                    <div class="price">$100.00</div>
-                    <div class="sale-price">$20.00</div>
+            </div>
+            <div class="tendencia">
+                <img src="https://static.vecteezy.com/system/resources/previews/008/520/693/non_2x/gray-sweater-mockup-hanging-file-png.png">
+                <div class="info">
+                    <div class="nombre">
+                        <h3>Sudadera Rosa</h3>
+                    </div>
+                    <p> $5000</p>
+                </div>
+            </div>
+            <div class="tendencia">
+                <img src="https://images.vexels.com/media/users/3/129408/isolated/preview/729a1419381b9d6572742b4ae641c7d6-vestido-de-mujer-negro.png">
+                <div class="info">
+                    <div class="nombre">
+                        <h3>Sudadera Rosa</h3>
+                    </div>
+                    <p> $5000</p>
                 </div>
             </div>
         </div>
-
-        <div class="lorem-ipsum">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed auctor, magna a ullamcorper laoreet, lorem velit elementum eros, ac accumsan ipsum libero sed velit. Sed eget sapien quis turpis cursus consectetur. Donec et nibh ipsum. Donec vestibulum, ex eget convallis rhoncus, sem nisi rhoncus nisi, a posuere velit nulla et ipsum.
-        </div>
-    </div>
+    </section>
 </body>
 </html>
