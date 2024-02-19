0a497b8224eab0ae56bdbd907512b3dddcf5aba0
CesJu <cesar.juarezcastellon@gmail.com>
2024-02-19 12:45:14 -0700

    'tablaProveedores'

--- database/migrations/2024_02_15_010720_create__categorias_table.php
+++ database/migrations/2024_02_15_010720_create__categorias_table.php
@@ -4,8 +4,6 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-use function Laravel\Prompts\table;
-
 return new class extends Migration
 {
     /**
@@ -13,12 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('productos', function (Blueprint $table) {
+        Schema::create('categorias', function (Blueprint $table) {
             $table->id();
-            $table->string("Nombre",200); // Nombre,length. El default es 255
-            $table->foreignId("categoria_id")->references('id')->on('categorias');
-            $table->text("Descripcion");
-            $table->unsignedDouble("Precio");
+            $table->string("nombre",50);
             $table->timestamps();
         });
     }
@@ -28,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('productos');
+        Schema::dropIfExists('categorias');
     }
 };
