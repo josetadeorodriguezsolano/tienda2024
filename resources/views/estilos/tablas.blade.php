aa28d67454a36b87bd42d99080666257889f1c92
iFeerx <fernando_akjs@hotmail.com>
2024-02-20 15:36:35 -0700

    Agregado estilos de tablas

--- resources/views/estilos/tablas.blade.php
+++ resources/views/estilos/tablas.blade.php
@@ -10,35 +10,6 @@
 @show
 
 
-<section class="tabla-basica">
-    <h1>Tabla básica</h1>
-    <table>
-        <thead>
-            <tr>
-                <th>NOMBRE</th>
-                <th>EDAD</th>
-                <th>CIUDAD</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td>ANA</td>
-                <td>25</td>
-                <td>MADRID</td>
-            </tr>
-            <tr>
-                <td>LUIS</td>
-                <td>32</td>
-                <td>BARCELONA</td>
-            </tr>
-            <tr>
-                <td>MARÍA</td>
-                <td>28</td>
-                <td>SEVILLA</td>
-            </tr>
-        </tbody>
-    </table>
-</section>
 
 
 
@@ -229,45 +200,194 @@
     <h1>Tabla separada</h1>
     <ul class="responsive-table">
         <li class="table-header">
-          <div class="col col-1">Id</div>
-          <div class="col col-2">Cliente</div>
-          <div class="col col-3">Deuda</div>
-          <div class="col col-4">Estado</div>
+            <div class="col col-1">Id</div>
+            <div class="col col-2">Cliente</div>
+            <div class="col col-3">Deuda</div>
+            <div class="col col-4">Estado</div>
         </li>
         <li class="table-row">
-          <div class="col col-1" data-label="Job Id">42235</div>
-          <div class="col col-2" data-label="Customer Name">John Doe</div>
-          <div class="col col-3" data-label="Amount">$350</div>
-          <div class="col col-4" data-label="Payment Status">Pendiente</div>
+            <div class="col col-1" data-label="Job Id">42235</div>
+            <div class="col col-2" data-label="Customer Name">John Doe</div>
+            <div class="col col-3" data-label="Amount">$350</div>
+            <div class="col col-4" data-label="Payment Status">Pendiente</div>
         </li>
         <li class="table-row">
-          <div class="col col-1" data-label="Job Id">42442</div>
-          <div class="col col-2" data-label="Customer Name">Jennifer Smith</div>
-          <div class="col col-3" data-label="Amount">$220</div>
-          <div class="col col-4" data-label="Payment Status">Pendiente</div>
+            <div class="col col-1" data-label="Job Id">42442</div>
+            <div class="col col-2" data-label="Customer Name">Jennifer Smith</div>
+            <div class="col col-3" data-label="Amount">$220</div>
+            <div class="col col-4" data-label="Payment Status">Pendiente</div>
         </li>
         <li class="table-row">
-          <div class="col col-1" data-label="Job Id">42257</div>
-          <div class="col col-2" data-label="Customer Name">John Smith</div>
-          <div class="col col-3" data-label="Amount">$341</div>
-          <div class="col col-4" data-label="Payment Status">Pendiente</div>
+            <div class="col col-1" data-label="Job Id">42257</div>
+            <div class="col col-2" data-label="Customer Name">John Smith</div>
+            <div class="col col-3" data-label="Amount">$341</div>
+            <div class="col col-4" data-label="Payment Status">Pendiente</div>
         </li>
         <li class="table-row">
-          <div class="col col-1" data-label="Job Id">42311</div>
-          <div class="col col-2" data-label="Customer Name">John Carpenter</div>
-          <div class="col col-3" data-label="Amount">$115</div>
-          <div class="col col-4" data-label="Payment Status">Pendiente</div>
+            <div class="col col-1" data-label="Job Id">42311</div>
+            <div class="col col-2" data-label="Customer Name">John Carpenter</div>
+            <div class="col col-3" data-label="Amount">$115</div>
+            <div class="col col-4" data-label="Payment Status">Pendiente</div>
         </li>
-      </ul>
+    </ul>
 </section>
 
+<section class="tabla-comparativa">
+
+    <h1>Tabla comparativa</h1>
+
+    <div class="table">
+        <div class="table-cell"></div>
+        <div class="table-cell plattform">
+            <h3>Gratis</h3>
+            <a href="" class="btn">Seleccionar</a>
+        </div>
+        <div class="table-cell enterprise">
+            <h3>Enterprise</h3>
+
+            <a href="" class="btn">Seleccionar</a>
+
+        </div>
+        <div class="table-cell cell-feature">Almacenamiento en la nube</div>
+        <div class="table-cell">
+            <svg width="18" height="18" viewBox="0 0 18 18" xmlns="http://www.w3.org/2000/svg">
+                <title>check_blue</title>
+                <path
+                    d="M6.116 14.884c.488.488 1.28.488 1.768 0l10-10c.488-.488.488-1.28 0-1.768s-1.28-.488-1.768 0l-9.08 9.15-4.152-4.15c-.488-.488-1.28-.488-1.768 0s-.488 1.28 0 1.768l5 5z"
+                    fill="limegreen" fill-rule="evenodd" />
+            </svg>
+        </div>
+        <div class="table-cell">
+            <svg class="enterprise-check" width="18" height="18" viewBox="0 0 18 18"
+                xmlns="http://www.w3.org/2000/svg">
+                <title>check_blue</title>
+                <path
+                    d="M6.116 14.884c.488.488 1.28.488 1.768 0l10-10c.488-.488.488-1.28 0-1.768s-1.28-.488-1.768 0l-9.08 9.15-4.152-4.15c-.488-.488-1.28-.488-1.768 0s-.488 1.28 0 1.768l5 5z"
+                    fill="limegreen" fill-rule="evenodd" />
+            </svg>
+        </div>
+        <div class="table-cell cell-feature">Integración con Office 365</div>
+        <div class="table-cell">
+            <svg width="18" height="18" viewBox="0 0 18 18" xmlns="http://www.w3.org/2000/svg">
+                <title>check_blue</title>
+                <path
+                    d="M6.116 14.884c.488.488 1.28.488 1.768 0l10-10c.488-.488.488-1.28 0-1.768s-1.28-.488-1.768 0l-9.08 9.15-4.152-4.15c-.488-.488-1.28-.488-1.768 0s-.488 1.28 0 1.768l5 5z"
+                    fill="limegreen" fill-rule="evenodd" />
+            </svg>
+        </div>
+        <div class="table-cell">
+            <svg class="enterprise-check" width="18" height="18" viewBox="0 0 18 18"
+                xmlns="http://www.w3.org/2000/svg">
+                <title>check_blue</title>
+                <path
+                    d="M6.116 14.884c.488.488 1.28.488 1.768 0l10-10c.488-.488.488-1.28 0-1.768s-1.28-.488-1.768 0l-9.08 9.15-4.152-4.15c-.488-.488-1.28-.488-1.768 0s-.488 1.28 0 1.768l5 5z"
+                    fill="limegreen" fill-rule="evenodd" />
+            </svg>
+        </div>
+        <div class="table-cell cell-feature">Chat y llamadas</div>
+        <div class="table-cell">
+            <svg width="18" height="18" viewBox="0 0 18 18" xmlns="http://www.w3.org/2000/svg">
+                <title>check_blue</title>
+                <path
+                    d="M6.116 14.884c.488.488 1.28.488 1.768 0l10-10c.488-.488.488-1.28 0-1.768s-1.28-.488-1.768 0l-9.08 9.15-4.152-4.15c-.488-.488-1.28-.488-1.768 0s-.488 1.28 0 1.768l5 5z"
+                    fill="limegreen" fill-rule="evenodd" />
+            </svg>
+        </div>
+        <div class="table-cell">
+            <svg class="enterprise-check" width="18" height="18" viewBox="0 0 18 18"
+                xmlns="http://www.w3.org/2000/svg">
+                <title>check_blue</title>
+                <path
+                    d="M6.116 14.884c.488.488 1.28.488 1.768 0l10-10c.488-.488.488-1.28 0-1.768s-1.28-.488-1.768 0l-9.08 9.15-4.152-4.15c-.488-.488-1.28-.488-1.768 0s-.488 1.28 0 1.768l5 5z"
+                    fill="limegreen" fill-rule="evenodd" />
+            </svg>
+        </div>
+        <div class="table-cell cell-feature">Videoconferencias</div>
+        <div class="table-cell">
+            <svg width="18" height="18" viewBox="0 0 18 18" xmlns="http://www.w3.org/2000/svg">
+                <title>check_blue</title>
+                <path
+                    d="M6.116 14.884c.488.488 1.28.488 1.768 0l10-10c.488-.488.488-1.28 0-1.768s-1.28-.488-1.768 0l-9.08 9.15-4.152-4.15c-.488-.488-1.28-.488-1.768 0s-.488 1.28 0 1.768l5 5z"
+                    fill="limegreen" fill-rule="evenodd" />
+            </svg>
+        </div>
+        <div class="table-cell">
+            <svg class="enterprise-check" width="18" height="18" viewBox="0 0 18 18"
+                xmlns="http://www.w3.org/2000/svg">
+                <title>check_blue</title>
+                <path
+                    d="M6.116 14.884c.488.488 1.28.488 1.768 0l10-10c.488-.488.488-1.28 0-1.768s-1.28-.488-1.768 0l-9.08 9.15-4.152-4.15c-.488-.488-1.28-.488-1.768 0s-.488 1.28 0 1.768l5 5z"
+                    fill="limegreen" fill-rule="evenodd" />
+            </svg>
+        </div>
+        <div class="table-cell cell-feature">Poder hacer capturas de pantalla</div>
+        <div class="table-cell"></div>
+        <div class="table-cell">
+            <svg class="enterprise-check" width="18" height="18" viewBox="0 0 18 18"
+                xmlns="http://www.w3.org/2000/svg">
+                <title>check_blue</title>
+                <path
+                    d="M6.116 14.884c.488.488 1.28.488 1.768 0l10-10c.488-.488.488-1.28 0-1.768s-1.28-.488-1.768 0l-9.08 9.15-4.152-4.15c-.488-.488-1.28-.488-1.768 0s-.488 1.28 0 1.768l5 5z"
+                    fill="limegreen" />
+            </svg>
+        </div>
+        <div class="table-cell cell-feature">Acceso a contenido 4K HDR</div>
+        <div class="table-cell"></div>
+        <div class="table-cell">
+            <svg class="enterprise-check" width="18" height="18" viewBox="0 0 18 18"
+                xmlns="http://www.w3.org/2000/svg">
+                <title>check_blue</title>
+                <path
+                    d="M6.116 14.884c.488.488 1.28.488 1.768 0l10-10c.488-.488.488-1.28 0-1.768s-1.28-.488-1.768 0l-9.08 9.15-4.152-4.15c-.488-.488-1.28-.488-1.768 0s-.488 1.28 0 1.768l5 5z"
+                    fill="limegreen" fill-rule="evenodd" />
+            </svg>
+        </div>
+        <div class="table-cell cell-feature">Sonido envolvente Dolby Atmos</div>
+        <div class="table-cell"></div>
+        <div class="table-cell">
+            <svg class="enterprise-check" width="18" height="18" viewBox="0 0 18 18"
+                xmlns="http://www.w3.org/2000/svg">
+                <title>check_blue</title>
+                <path
+                    d="M6.116 14.884c.488.488 1.28.488 1.768 0l10-10c.488-.488.488-1.28 0-1.768s-1.28-.488-1.768 0l-9.08 9.15-4.152-4.15c-.488-.488-1.28-.488-1.768 0s-.488 1.28 0 1.768l5 5z"
+                    fill="limegreen" fill-rule="evenodd" />
+            </svg>
+        </div>
+        <div class="table-cell cell-feature">Atención a cliente 24/7 </div>
+        <div class="table-cell"></div>
+        <div class="table-cell">
+            <svg class="enterprise-check" width="18" height="18" viewBox="0 0 18 18"
+                xmlns="http://www.w3.org/2000/svg">
+                <title>check_blue</title>
+                <path
+                    d="M6.116 14.884c.488.488 1.28.488 1.768 0l10-10c.488-.488.488-1.28 0-1.768s-1.28-.488-1.768 0l-9.08 9.15-4.152-4.15c-.488-.488-1.28-.488-1.768 0s-.488 1.28 0 1.768l5 5z"
+                    fill="limegreen" fill-rule="evenodd" />
+            </svg>
+        </div>
+
+        <div class="table-cell cell-feature">Precio </div>
+        <div class="table-cell">
+            <div>0.00MXN</div>
+          <div class="info">(Sujeto a cualquier cambio)</div>
+
+        </div>
+        <div class="table-cell">
+          <div> 8,495.39MXN (mensual)</div>
+          <div class="info">(No aplica IVA)</div>
+
+
+
+        </div>
+
+
+    </div>
 
 
 
 
-@section('aside')
-@section('footer')
-    @include('layouts.componentes.footer')
-@show
 
 
+    @section('aside')
+    @section('footer')
+        @include('layouts.componentes.footer')
+    @show
