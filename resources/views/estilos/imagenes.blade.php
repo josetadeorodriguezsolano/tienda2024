a44be271cb43ea0b2759233e69d41517376fe70a
KEVIN-OA <Kevinosuna411@gmail.com>
2024-02-19 22:10:53 -0700

    imagenes

--- resources/views/estilos/imagenes.blade.php
+++ resources/views/estilos/imagenes.blade.php
@@ -3,10 +3,7 @@
 @section('main')
     <div>
         <img src="../img/logo.jpg" class="imagen">
-        <br><br>
-        <img src="../img/logo.jpg" class="imagen2">
-        <br><br>
-        <img src="../img/logo.jpg" class="imagen3">
+
         <br><br>
         <img src="../img/logo.jpg" class="imagen4">
         <br><br>
@@ -14,6 +11,13 @@
         <br><br>
         <img src="../img/logo.jpg" class="imagen6">
         <br><br>
+        <img src="../img/logo.jpg" class="imagen2">
+        <br><br>
         <img src="../img/logo.jpg" class="imagen7">
-
     </div>
+    <br><br><br><br>
+    <div class="contenedor">
+        <a href="#imagenes" class="imagenes-link">
+          <img src="../img/logo.jpg" alt="Imagenes" class="imagenes imagen" id="imagenes">
+        </a>
+      </div>
