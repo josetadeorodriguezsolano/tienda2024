0e9a731f459c99669d5e9d117b6f7a82080f4934
CesJu <cesar.juarezcastellon@gmail.com>
2024-02-21 09:54:57 -0700

    'claseDB'

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,14 +13,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
-
-        //orden en que se debe ejecutar  (no se puede tener productos sin categorias)
-        $this->call([CategoriaSeeder::class,ProductoSeeder::class]);
+        $this->call([
+            CategoriasSeeder::class,
+            ProductosSeeder::class,
+    ]);
     }
 }
