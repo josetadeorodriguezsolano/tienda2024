0972608dbff7ff9757679df44d89a14a85b38a6e
CesJu <cesar.juarezcastellon@gmail.com>
2024-02-21 09:54:57 -0700

    'claseDB'

--- resources/views/principal.blade.php
+++ resources/views/principal.blade.php
@@ -1,4 +1,9 @@
 
+@extends('layouts.plantilla')
+@section('titulo','tarjetas')
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+@section('main')
+    @vite(['resources/sass/global/tarjetas.scss', 'resources/js/app.js'])
     <header id="encabezadoPrincipal">
         <img src="img/logo.jpg">
         Super Tienda en Linea
@@ -24,8 +29,7 @@
         Categorias mas buscadas
     </section>
     <section id="anuncios">
-        <p> Hola soy Cesar</p>
-        <p>Hola soy Fernando</p>
+
         Anuncios
     </section>
     <section id="superOfertas">
@@ -38,7 +42,28 @@
         Lo Nuevo
     </section>
     <section id="gamers">
-        Gamers
+        <section id="tarjeta01">
+            <div class="tarjeta">
+                <div class="imgTarjeta">
+                    <img class="imgTarjeta" src="img/producto1.webp" alt="Producto">
+                </div>
+                <div class="contenidoTarjeta">
+                    <p class="tituloTarjeta" title="HP Laptop PC 15s-fq5021sa | Intel Core i5-1235U Processor | 8GB RAM | 256GB SSD | Intel UHD Graphics | Windows 11 Home | Natural Silver">HP Laptop PC 15s-fq5021sa | Intel Core i5-1235U Processor | 8GB RAM | 256GB SSD | Intel UHD Graphics | Windows 11 Home | Natural Silver</p>
+                    <br><span class="precioTarjeta">MX$5,999.00</span>
+                    <div class="detallesProducto">
+                        <p>A 12 meses sin intereses de $399 <br>
+                            Envío gratis
+                        </p>
+                        <div class="ratingTarjeta">
+                            ★★★★☆
+                        </div>
+                    </div>
+                </div>
+                <div class="butonTarjeta">
+                    <button>Agregar</button>
+                </div>
+            </div>
+        </section>
     </section>
     <section id="comentarios">
         Comentarios
