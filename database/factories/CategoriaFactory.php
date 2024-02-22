77f0058c7b0196cc16176fc4089b214f43b0a3e2
Elian <ElianOsirisLunaGaxiola@gmail.com>
2024-02-21 18:20:19 -0700

    Trabajo actual

--- database/factories/CategoriaFactory.php
+++ database/factories/CategoriaFactory.php
@@ -2,6 +2,8 @@
 
 namespace Database\Factories;
 
+
+use Doctrine\Inflector\Rules\Word;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -18,34 +20,7 @@ class CategoriaFactory extends Factory
     {
         return [
             //id automatico
-            'nombre'=>fake()->Word(),
-        ];
-    }
-}
-
-<?php
-
-namespace Database\Factories;
-
-use Illuminate\Database\Eloquent\Factories\Factory;
-
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Categoria>
- */
-class CategoriaFactory extends Factory
-{
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
-    public function definition(): array
-    {
-        return [
-
-         'nombre' => fake()->Word(),
-
-
+            "nombre"=>fake()->Word(),
         ];
     }
 }
