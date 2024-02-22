0695de503e98470b4cf7e4baa5796fa4813cb999
tadeo.rodriguez <tadeo.rs@lapaz.tecnm.mx>
2024-02-21 18:54:51 -0700

    Controllers, Paginacion

--- app/Http/Controllers/PrincipalController.php
+++ app/Http/Controllers/PrincipalController.php
@@ -16,18 +16,3 @@ class PrincipalController extends Controller
         return view('principal',['productos'=>$categoria->productosPaginados()]);
     }
 }
-
-<?php
-
-namespace App\Http\Controllers;
-
-use App\Models\Categoria;
-use Illuminate\Http\Request;
-
-class PrincipalController extends Controller
-{
-    public static function paginaPrincipal(){
-        $categoria = Categoria::buscarPorNombre('gamers');
-        return view('principal', ['productos' => $categoria -> productos]);
-    }
-}
--- app/Models/Categoria.php
+++ app/Models/Categoria.php
@@ -14,7 +14,19 @@ class Categoria extends Model
         return $this->hasMany(Producto::class,'categoria_id','id');
     }
 
-    public static function buscarPorNombre($nombre){
+    public function productos()
+    {
+
+        return $this->hasMany(Producto::class,'categoria_id','id');
+    }
+
+    public static function buscarPorNombre($nombre)
+    {
         return Categoria::where('nombre',$nombre)->get()->first();
+    }
+
+    public function productosPaginados()
+    {
+        return Producto::where('categoria_id',$this->id)->paginate(9);
     }
 }
