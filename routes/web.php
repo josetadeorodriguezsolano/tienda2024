25ab10ac5e29778a0f85c79732e66106fe16f188
tadeo.rodriguez <tadeo.rs@lapaz.tecnm.mx>
2024-02-27 17:35:34 -0700

    nada

--- routes/web.php
+++ routes/web.php
@@ -4,22 +4,15 @@ use App\Http\Controllers\PrincipalController;
 use App\Http\Controllers\ProductosController;
 use App\Http\Controllers\UsersController;
 use Illuminate\Support\Facades\Route;
+use PrincipalController
 
-Route::get      ('/',[PrincipalController::class, 'paginaPrincipal'])
-->name('principal');
-
-Route::prefix('users')->controller(UsersController::class)
-    ->group(function(){
-        Route::get('/log','vistaLogin');
-        Route::post('login','login');
-    });
+Route::get('/',[PrincipalController::class,'principal']);
 
 Route::prefix('productos')->controller(ProductosController::class)
     ->group(function(){
         Route::get('formAlta','formAlta');
-        Route::post('alta','alta');
+        Route::get('alta','alta');
     });
-
 
 
 
