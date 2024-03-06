dced975941442d1619dea8f12b482e9db3c1f749
tadeo.rodriguez <tadeo.rs@lapaz.tecnm.mx>
2024-03-05 17:15:08 -0700

    nada

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,7 @@ Route::prefix('productos')->controller(ProductosController::class)
     ->group(function(){
         Route::get('formAlta','formAlta');
         Route::post('alta','alta');
+        Route::get('show10','show10');
     });
 
 
