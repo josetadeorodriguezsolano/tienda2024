cdde0ca8c945f53aa127d5a43112ce84cbe823d9
tadeo.rodriguez <tadeo.rs@lapaz.tecnm.mx>
2024-03-05 18:45:23 -0700

    Paises

---
+++ database/seeders/PaisSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class paisSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::unprepared(file_get_contents(base_path().'/database/sql/paises.sql'));
+        DB::unprepared(file_get_contents(base_path().'/database/sql/estados.sql'));
+        DB::unprepared(file_get_contents(base_path().'/database/sql/municipios.sql'));
+        DB::unprepared(file_get_contents(base_path().'/database/sql/ciudades.sql'));
+        DB::unprepared(file_get_contents(base_path().'/database/sql/asentamientos.sql'));
+        DB::unprepared(file_get_contents(base_path().'/database/sql/localidades.sql'));
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,43 @@
 
 use App\Http\Controllers\PrincipalController;
 use App\Http\Controllers\ProductosController;
+use App\Http\Controllers\UsersController;
+use App\Http\Middleware\LoginMiddleware;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/',[PrincipalController::class, 'paginaPrincipal']);
-Route::get('/', [PrincipalController::class,'paginaPrincipal']);
+Route::get('/',[PrincipalController::class, 'paginaPrincipal'])
+->name('principal');
+
+Route::prefix('users')->controller(UsersController::class)
+    ->group(function(){
+        Route::get('login','vistaLogin');
+        Route::post('login','login');
+    });
+
+Route::prefix('producto')->controller(ProductosController::class)
+    ->group(function(){
+        Route::get('formAlta','formAlta')->name('altaProductos');
+        Route::post('alta','alta');
+    })->middleware('login');
+Route::middleware(['login'])->get('/productos/formAlta',[ProductosController::class,'formAlta'])->name('altaProductos');
+
+
+
+
+
+
+Route::prefix('productos')->controller(ProductosController::class)
+    ->group(function(){
+        Route::get('formAlta','formAlta');
+        Route::post('alta','alta');
+        Route::get('show10','show10');
+    });
+
+
+
+
+
+
 
 Route::get('/estilos/banners',function () {
     return view('estilos.banners');
